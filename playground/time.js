58f57d4f1cbb96701f78f4e20657cc902e792cfd
Kocan Irvin <irvin.kocan@cylo.com>
2018-08-28 15:16:16 +0200

    Format timestamps using momentjs

---
+++ playground/time.js
@@ -0,0 +1,8 @@
+var moment = require('moment');
+
+// var date = new Date();
+// var months = ['Jan', 'Feb'];
+// console.log(date.getMonth());
+
+var date = new moment();
+console.log(date.format('MMM Do, YYYY'));
